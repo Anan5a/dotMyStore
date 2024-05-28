ab5297c4246843d4f6938bb23c9554f41b3fc0d4
Sayem Prodhan <ananta.ap69@gmail.com>
2024-05-28 16:28:36 +0600

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+        public decimal Weight { get; set; }
+
+
+    }
+}
---
+++ Models/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Models
+{
+    public class ProductImage
+    {
+        public long Id { get; set; }
+        public string ImageUrl { get; set; }
+        public long ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        
+    }
+}
